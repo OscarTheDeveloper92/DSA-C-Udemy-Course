21c8dcb997ea0036a07bb1cbc36c99a2e4c388d1
OscarTheDeveloper92 <129730643+OscarTheDeveloper92@users.noreply.github.com>
2024-12-18 10:48:19 -0800

    Array basics
    Going over how to declare and initialize an array as well as using a for and for each loop to initialize as well.

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	//integers take up 4 bytes of memory
+	int A[10] = {2,4,6,8,10,12};
+
+	/* Regular for loop
+	for (int i = 0; i < 10; i++)
+	{
+		cout << A[i] << endl;
+	}
+	*/
+
+	/* for each loop
+	for(x: A){
+		cout << x << endl;
+	}
+	*/
+	
+
+	return 0;
+}
